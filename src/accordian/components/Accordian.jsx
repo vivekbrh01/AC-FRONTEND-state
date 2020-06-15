c10763020c64a62c96a53f4ca951de1431ea2a8a
Vivek Baruah <vivekbrh01@gmail.com>
2020-06-15 19:46:46 +0530

    Accordian bug fixed

--- src/accordian/components/Accordian.jsx
+++ src/accordian/components/Accordian.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 
 class Accordian extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
   }
   render() {
     return (
       <section className="section">
         <h3> {this.props.title}</h3>
-        <p> {this.props.text}</p>
+        {(this.props.isClicked
+          ? <p> {this.props.text}</p> : "")}
       </section>
     )
   }
